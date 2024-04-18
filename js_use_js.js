d7ef6ea8533565e2286034e51a84231ab124ae94
Harsh Shrivastava <141989341+HARSHSHRI12@users.noreply.github.com>
2024-04-19 00:19:06 +0530

    Add files via upload

---
+++ js_use_js.js
@@ -0,0 +1,23 @@
+
+/*<html>
+<head>
+<script type="text/javascript" src="massage.js"></script>
+</head>
+<body>
+<h1>what is your name?</h1>
+<p>my name is harsh</p>
+<input type="button" value="click" onclick="msg()"/>
+</body>
+</html>-->*/
+
+<html>  
+<head>  
+<script type="text/javascript" src="message.js"></script>  
+</head>  
+<body>  
+<p>Welcome to JavaScript</p>  
+<form>  
+<input type="button" value="click" onclick="msg()"/>  
+</form>  
+</body>  
+</html>  
